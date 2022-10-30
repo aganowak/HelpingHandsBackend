d8eaafcc8566467d43f5e25629e6546a7c4bd874
aganowak <agnieszka.nowak@gmx.com>
2022-10-30 22:14:59 +0100

    /events endpoint works

--- src/main/java/com.codecool.helpinghands/controller/EventController.java
+++ src/main/java/com.codecool.helpinghands/controller/EventController.java
@@ -1,5 +1,8 @@
 package com.codecool.helpinghands.controller;
 
+import com.codecool.helpinghands.model.Event;
+import com.codecool.helpinghands.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,22 @@ import java.util.List;
 @RestController
 public class EventController {
 
-    @GetMapping(path = "/home")
+    private EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events")
+    public List<Event> getEvents(){
+        return eventService.getEvents();
+    }
+
+
+
+    @GetMapping(path = "/")
     public String mainPage(){
-        return "Helping hands main page";
+        return "This is Helping Hands app";
     }
 }
