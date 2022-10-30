d221c32db80864db461a8550e81bd35966e812f0
aganowak <agnieszka.nowak@gmx.com>
2022-10-30 21:06:53 +0100

    Slot model created

--- src/main/java/com.codecool.helpinghands/model/Slot.java
+++ src/main/java/com.codecool.helpinghands/model/Slot.java
@@ -1,4 +1,18 @@
 package com.codecool.helpinghands.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Slot {
+    private int slotId;
+    private int eventId;
+    private LocalDateTime slotStartTime;
+    private LocalDateTime slotEndTime;
+    private int numOfVolunteers;
 }
