8c1ca54591b89d3d0dd8609c5ff71794a1caefc9
aganowak <agnieszka.nowak@gmx.com>
2022-11-04 11:45:36 +0100

    unused code removed

--- src/main/java/com.codecool.helpinghands/controller/UserController.java
+++ src/main/java/com.codecool.helpinghands/controller/UserController.java
@@ -24,31 +24,6 @@ public class UserController {
         this.eventService = eventService;
     }
 
-//    @GetMapping("/users")
-//    public List<User> getUsers(){
-//        return userService.getUsers();
-//    }
-//
-//    // curl -d "eventId=1444" -X POST -H "Content-Type: application/x-www-form-urlencoded" http://localhost:8080/user/0/assignEvent
-//    @PostMapping("/user/{userId}/assignEvent")
-//    public String assignEvent(@PathVariable("userId") int userId, @RequestParam int eventId){
-//
-//        // do the assignment
-//        User selected = null;
-//        for (var user : userService.getUsers()) {
-//            if (user.getUserId() == userId)
-//            {
-//                selected = user;
-//                break;
-//            }
-//        }
-//        if (selected == null)
-//            return "Error: User not found!";
-//        selected.assign(eventId);
-//        return "User %d assigned to event %d".formatted(selected.getUserId(), eventId);
-//    }
-
-
     @PostMapping("/users/assign/{eventId}")
     public void assignUserToEvent(@PathVariable("eventId") int eventId){
         User loggedInUser = userService.getUserById(1).get();
