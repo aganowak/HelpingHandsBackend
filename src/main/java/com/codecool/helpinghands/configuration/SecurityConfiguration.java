01468a797bcdffc0afd0cf41d3409001eb3c6dd7
aganowak <agnieszka.nowak@gmx.com>
2022-11-28 11:13:24 +0100

    USER authority granted to a user that logs in

--- src/main/java/com/codecool/helpinghands/configuration/SecurityConfiguration.java
+++ src/main/java/com/codecool/helpinghands/configuration/SecurityConfiguration.java
@@ -24,6 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/events").hasRole("USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
